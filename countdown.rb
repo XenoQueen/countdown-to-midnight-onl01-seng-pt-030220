25d679de89722ceddcc9039501915573b3c2c675
Maika Kailani <queenmab051@gmail.com>
2020-03-20 23:31:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,11 @@
 def countdown(number)
   number = 10
   while number > 0
-  puts "#{number} SECOND(S)!"
+    puts "#{number} SECOND(S)!"
   number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep
   
-end
